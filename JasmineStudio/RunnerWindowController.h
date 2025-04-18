f03c1e3153b6d200255542792d5ac5f38cf9b7fe
Steve Gravrock <sdg@panix.com>
2025-04-18 13:39:06 -0700

    Basic table view

--- JasmineStudio/RunnerWindowController.h
+++ JasmineStudio/RunnerWindowController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface RunnerWindowController : NSWindowController
+@interface RunnerWindowController : NSWindowController<NSTableViewDelegate, NSTableViewDataSource>
 
 - (void)initProjectWithBaseDir:(NSURL *)baseDir;
 
--- JasmineStudio/RunnerWindowController.m
+++ JasmineStudio/RunnerWindowController.m
@@ -7,6 +7,32 @@
 
 #import "RunnerWindowController.h"
 
+// Model for table view cells
+// To bind:
+// * Select the text field inside the Table Cell View in Interface Builder
+// * Select the chain-link tab in the inspector
+// * Expand Value
+// * Check Bind to
+// * Make sure Table Cell View is selected in the dropdown
+// * Set Model Key Path to objectValue.stringValue
+// (Existing documentation for this is a little off due to changes in Xcode.)
+@interface TempModel: NSObject<NSCopying>
+@property (nonatomic, assign) NSInteger n;
+- (NSString *)stringValue;
+@end
+
+@implementation TempModel
+- (NSString *)stringValue {
+    return [NSString stringWithFormat:@"%ld", (long)self.n];
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+    TempModel *copy = [[[self class] alloc] init];
+    copy.n = self.n;
+    return copy;
+}
+@end
+
 @interface RunnerWindowController ()
 
 @property (nonatomic, strong) NSURL *baseDir;
@@ -16,6 +42,8 @@
 @end
 
 @implementation RunnerWindowController
+
+#pragma mark Initialization
 
 - (NSNibName)windowNibName {
     return @"RunnerWindowController";
@@ -35,6 +63,20 @@
     if (self.baseDir && self.windowLoaded) {
         self.window.title = [self.baseDir lastPathComponent];
     }
+}
+
+
+#pragma mark NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return 3;
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    TempModel *m = [[TempModel alloc] init];
+    m.n = row;
+    NSLog(@"Returning %p for %ld", m, (long)row);
+    return m;
 }
 
 @end
