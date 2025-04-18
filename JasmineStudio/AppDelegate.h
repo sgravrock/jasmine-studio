f684a6fb12a743ecff0dbbff5f47d960f358d747
Steve Gravrock <sdg@panix.com>
2025-04-18 09:08:26 -0700

    Initial Commit

---
+++ JasmineStudio/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JasmineStudio
+//
+//  Created by Stephen Gravrock on 4/18/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ JasmineStudio/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  JasmineStudio
+//
+//  Created by Stephen Gravrock on 4/18/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
