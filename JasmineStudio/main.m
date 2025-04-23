7255c731b4713b27d71fa5096392054a3aa96d49
Steve Gravrock <sdg@panix.com>
2025-04-22 19:45:27 -0700

    Prevent deadlock when more than one unit test class is run

--- JasmineStudio/main.m
+++ JasmineStudio/main.m
@@ -11,5 +11,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
     }
+    
+    // XCTestBundlePath
     return NSApplicationMain(argc, argv);
 }
